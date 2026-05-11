15ae267e90b8b48a7acf992364975263314f82e4
= <scvfufa@gmail.com>
2026-05-11 18:19:38 +0900

    feat:: PlayerCursor 클래스 생성(UUserWidget 상속)

---
+++ Source/miniProject/Public/PlayerCursor.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PlayerCursor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MINIPROJECT_API UPlayerCursor : public UUserWidget
+{
+	GENERATED_BODY()
+	
+	
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UImage* cursorImage;
+	
+};

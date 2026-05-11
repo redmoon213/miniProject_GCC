196fb1c085cc96f721e8abd3cb8800da3e79fd7b
= <scvfufa@gmail.com>
2026-05-11 15:36:47 +0900

    feat:: PlayerPawn 클래스 생성(Pawn 상속), BoxComponent와 StaticMeshComponent 기초 세팅 (헤더 및 생성자)

---
+++ Source/miniProject/Private/PlayerPawn.cpp
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "miniProject/Public/PlayerPawn.h"
+
+#include "Components/BoxComponent.h"
+
+
+// Sets default values
+APlayerPawn::APlayerPawn()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	boxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("My Box Component"));
+	SetRootComponent(boxComp);
+	FVector boxSize = FVector(50, 50, 50);
+	boxComp->SetBoxExtent(boxSize);
+	
+	
+	meshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("My StaticMesh Component"));
+	boxComp->SetupAttachment(meshComp);
+
+
+}	
+
+// Called when the game starts or when spawned
+void APlayerPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlayerPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void APlayerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	
+	
+}
+

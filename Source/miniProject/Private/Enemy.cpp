20db263b4b9bb0e8b7881081713facab4f3db9fe
= <scvfufa@gmail.com>
2026-05-12 13:50:03 +0900

    feat:: Enemy 클래스 생성 및 diceOffset 설정, UpdateDiceEye 함수 작성(눈금 업데이트 함수)

---
+++ Source/miniProject/Private/Enemy.cpp
@@ -0,0 +1,55 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Enemy.h"
+
+#include "Components/BoxComponent.h"
+
+
+// Sets default values
+AEnemy::AEnemy()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	boxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("My Box Component"));
+	SetRootComponent(boxComp);
+	FVector boxSize = FVector(50.f, 50.f, 50.f);
+	boxComp->SetBoxExtent(boxSize);
+	
+	meshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("My Mesh Component"));
+	meshComp->SetupAttachment(boxComp);
+	
+		
+}
+
+// Called when the game starts or when spawned
+void AEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	UMaterialInterface* baseMaterial = meshComp->GetMaterial(0);
+	if (baseMaterial!=nullptr)
+	{
+		dynamicMaterial = meshComp->CreateDynamicMaterialInstance(0, baseMaterial);
+		
+	}
+	
+}
+
+// Called every frame
+void AEnemy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AEnemy::UpdateDiceEye()
+{
+	if (dynamicMaterial!= nullptr && currentDiceEye >=1)
+	{
+		FVector2D UVindex = diceOffset[currentDiceEye-1];
+		
+		dynamicMaterial->SetScalarParameterValue("uIndex", UVindex.X);
+		dynamicMaterial->SetScalarParameterValue("vIndex", UVindex.Y);
+	}
+}
---
+++ Source/miniProject/Public/Enemy.h
@@ -0,0 +1,54 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Enemy.generated.h"
+
+UCLASS()
+class MINIPROJECT_API AEnemy : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AEnemy();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	UPROPERTY(EditAnywhere)
+	class UBoxComponent* boxComp;
+	
+	UPROPERTY(EditAnywhere)
+	class UStaticMeshComponent* meshComp;
+	
+	UPROPERTY(EditAnywhere)
+	class UMaterialInstanceDynamic* dynamicMaterial;
+	
+	UPROPERTY(EditAnywhere);
+	int32 currentDiceEye = 5;
+	
+	
+	//눈금 UV 오프셋
+	
+	const FVector2D diceOffset[6] = {
+		FVector2D(0.4f, 0.523f),	//1번 눈금 
+		FVector2D(0.65f, 0.023f),   // 2번 눈금
+		FVector2D(0.4f, 0.273f),    // 3번 눈금
+		FVector2D(0.4f, 0.773f),	// 4번 눈금
+		FVector2D(0.15, 0.023f),	// 5번 눈금, 좌상단
+		FVector2D(0.4f, 0.023f)		// 6번 눈금
+	};
+	
+	
+	
+	
+	void UpdateDiceEye();
+};

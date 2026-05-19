5f7601bb3e7c7fd2ac8c4e238626a20731777e6e
= <scvfufa@gmail.com>
2026-05-19 17:12:14 +0900

    feat:: EnemyBossSpawnIndicator 클래스 생성(UUserWidget)

---
+++ Source/miniProject/Public/EnemyBossSpawnIndicator.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "EnemyBossSpawnIndicator.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MINIPROJECT_API UEnemyBossSpawnIndicator : public UUserWidget
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, meta=(BindWidget))
+	class UImage* warningIcon;
+};
